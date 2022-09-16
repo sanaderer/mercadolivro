9b31dae300cf5fc433398c0ee8178a2f7fb2ee82
sanaderer <sabrinanaderer@hotmail.com>
2022-09-16 16:42:55 -0300

    :tada: Initial commit

---
+++ src/main/kotlin/com/mercadolivro/mercadolivro/controller/request/PostCustomerRequest.kt
@@ -0,0 +1,8 @@
+package com.mercadolivro.mercadolivro.controller.request
+
+data class PostCustomerRequest (
+    var name: String,
+
+    var email: String,
+
+)
---
+++ src/main/kotlin/com/mercadolivro/mercadolivro/controller/request/PutCustomerRequest.kt
@@ -0,0 +1,8 @@
+package com.mercadolivro.mercadolivro.controller.request
+
+data class PutCustomerRequest (
+    var name: String,
+
+    var email: String,
+
+)
